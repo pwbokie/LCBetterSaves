940302d205614e228766377c03ab0491b9cd0ff4
Paul Merkamp <pmerkamp@butler.edu>
2023-11-20 12:32:30 -0500

    version 1.1.0

--- LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -4,6 +4,6 @@ namespace LCBetterSaves
     {
         public const string PLUGIN_GUID = "LCBetterSaves";
         public const string PLUGIN_NAME = "LCBetterSaves";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.1.0";
     }
 }
